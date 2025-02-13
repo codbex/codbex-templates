ed329cf3077ba44374bbb0a706535608f0fa36b3
NicoleNG18 <nicole.georgieva18@abv.bg>
2025-02-13 17:05:56 +0200

    add sales and purchase orders

---
+++ codbex-templates/print/sales-order-print-template.ts
@@ -0,0 +1,28 @@
+import { database } from "sdk/db";
+import { response } from "sdk/http";
+
+const SALES_ORDER_ID = 4;
+
+response.setContentType("text/html");
+
+let connection = database.getConnection("DefaultDB");
+try {
+    let statement = connection.prepareStatement(`SELECT * FROM "CODBEX_DOCUMENTTEMPLATE" WHERE "DOCUMENTTEMPLATE_TYPE" = ${SALES_ORDER_ID}`);
+    let resultSet = statement.executeQuery();
+    while (resultSet.next()) {
+        response.println(resultSet.getClob("DOCUMENTTEMPLATE_CONTENT"));
+    }
+    resultSet.close();
+    statement.close();
+} catch (e) {
+    if (e instanceof Error) {
+        console.error(e);
+        response.println(e.message);
+    } else {
+        console.error("Something went wrong", e);
+    }
+} finally {
+    connection.close();
+}
+response.flush();
+response.close();
